8f998ac2dd28edc8256a9757e59a48fd3885ca8c
Pooh Purinat <Pooh44578@gmail.com>
2024-12-30 15:34:19 +0700

    Add files via upload

---
+++ simple_calculator/main.py
@@ -0,0 +1,92 @@
+from tkinter import *
+
+#ฟังก์ชั่นกดปุ่ม
+def buttonClick(number):
+    global operator
+    operator = operator + str(number)
+    input_value.set(operator)
+
+#ฟังก์ชั่นเคลียค่า
+def buttonClear():
+    global operator
+    operator = ""
+    input_value.set("")
+
+#ฟังก๋ชั่นคำนวณผลลัพธ์
+def buttonEqual():
+    global operator
+    result = str(eval(operator)) #eval ใช้คำนวณ+-*/ทั่วไป
+    input_value.set(result)
+    operator = ""
+
+main = Tk()
+main.title("Simple Calculator")
+main.resizable(False,False)
+
+#variable
+operator = "" 
+input_value = StringVar()
+display_text = Entry(main, font=("arial",20,"bold"),textvariable=input_value,bd=30,
+                     insertwidth=4,bg="#fff", justify=RIGHT)
+
+#row0
+display_text.grid(columnspan=4)
+
+#สร้าง row1 บน cal เลข 1 7 8 9 +
+
+btn_7 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="7",command=lambda: buttonClick(7)) #command ส่งค่า
+btn_7.grid(row=1,column=0)
+
+btn_8 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="8",command=lambda: buttonClick(8))
+btn_8.grid(row=1,column=1)
+
+btn_9 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="9",command=lambda: buttonClick(9))
+btn_9.grid(row=1,column=2)
+
+btn_plus = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="+",command=lambda: buttonClick("+"))
+btn_plus.grid(row=1,column=3)
+
+#row 2 4 5 6 -
+
+btn_4 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="4",command=lambda: buttonClick(4))
+btn_4.grid(row=2,column=0)
+
+btn_5 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="5",command=lambda: buttonClick(5))
+btn_5.grid(row=2,column=1)
+
+btn_6 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="6",command=lambda: buttonClick(6))
+btn_6.grid(row=2,column=2)
+
+btn_sub = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="-",command=lambda: buttonClick("-"))
+btn_sub.grid(row=2,column=3)
+
+#row 3 1 2 3 *
+
+btn_1 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="1",command=lambda: buttonClick(1))
+btn_1.grid(row=3,column=0)
+
+btn_2 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="2",command=lambda: buttonClick(2))
+btn_2.grid(row=3,column=1)
+
+btn_3 = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="3",command=lambda: buttonClick(3))
+btn_3.grid(row=3,column=2)
+
+btn_mul = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="*",command=lambda: buttonClick("*"))
+btn_mul.grid(row=3,column=3)
+
+#row 4 C 0 = /
+
+btn_clear = Button(main,padx=16,bd=8,fg="#fff",bg="#3697f5",font=("arial",20,"bold"),text="C",command=buttonClear)
+btn_clear.grid(row=4,column=0)
+
+btn_0 = Button(main,padx=16,bd=8,fg="#fff",bg="#000",font=("arial",20,"bold"),text="0",command=lambda: buttonClick(0))
+btn_0.grid(row=4,column=1)
+
+btn_equal = Button(main,padx=16,bd=8,fg="#fff",bg="#fe9037",font=("arial",20,"bold"),text="=",command=buttonEqual)
+btn_equal.grid(row=4,column=2)
+
+btn_div = Button(main,padx=16,bd=8,fg="black",font=("arial",20,"bold"),text="/",command=lambda: buttonClick("/"))
+btn_div.grid(row=4,column=3)
+
+
+main.mainloop()
